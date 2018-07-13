26ebe171c0ef348522fbe879c97d954e2dfe2242
a belov <a.belov@education.merann.ru>
2018-07-13 19:52:32 +0300

    добавил main1...main5

---
+++ src/main2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "task2.h"
+using namespace std;
+
+/*
+int main()
+{
+	unsigned int value = 0;
+	cin >> value;
+	if (checkPrime(value))
+	{
+		cout << "value " << value<< " is Prime"<< endl;
+		cout << "the next Prime number is " << nextPrime(value)<< endl;
+	}
+	else
+		cout << "value " << value << " is NOT Prime" << endl;
+
+	cout << "Prime with number " << value << " = " << nPrime(value);
+	return 0;
+}
+//*/
---
+++ src/main3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "task3.h"
+using namespace std;
+/*
+int main()
+{
+	unsigned int value = 0;
+	cin >> value;
+	cout << "Summ = " << sumPrime(value)<< endl;
+	return 0;
+}
+//*/
