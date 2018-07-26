0ff66c8275493442d97af7307236a8a36774fd3f
Andrey <38911974+Barzabel@users.noreply.github.com>
2018-07-26 17:08:05 +0300

    initial commit

---
+++ src/task1.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef _TASK1_H_
+#define _TASK1_H_
+void prost(unsigned int *&arr, int &size2, const unsigned int vol);
+void push_back(unsigned int *&arr, int &size, const unsigned int value);
+unsigned long findValue(unsigned int min, unsigned int max);
+
+#endif
