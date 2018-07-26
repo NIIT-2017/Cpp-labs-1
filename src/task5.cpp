8ee54cfdb91211e5534dc00cab607d824229420c
Anna Pershina <7110575@mail.ru>
2018-07-26 18:06:26 +0300

    task5 corrected

--- src/task5.cpp
+++ src/task5.cpp
@@ -3,20 +3,24 @@
 #include <string>
 #include "task5.h"
 using namespace std;
-void split(char ***result, int *N, char *buf, char ch)
+
+	void split(char ***result, int *N, char *buf, char ch)
 {
 	int i = 0;
 	int count = 0;
 	*N = 0;
+	int begin = 0;
 	char *newBuf = new char[strlen(buf)];
 	strcpy(newBuf, buf);
 
 	while (newBuf[i] == ch)
+	{
 		i++;
-	int begin = i + 1;
+	}
+	if (i != 0) begin = i + 1;
 	while (newBuf[i] != '\0')
 	{
-		if (newBuf[i] == ch && newBuf[i + 1] != 0)
+		if (newBuf[i] == ch && newBuf[i + 1] != ch)
 			count++;
 		i++;
 	}
@@ -27,24 +31,21 @@ void split(char ***result, int *N, char *buf, char ch)
 
 	char **str = new char*[*N];
 	int numRes = 0;
-	int end = begin;
-	while (newBuf[i] != '\0')
+	while (*newBuf != '\0')
 	{
-		while (newBuf[i] != ch)
+		while (*newBuf == ch)
 		{
-			end++;
+			newBuf++;
 		}
-		if (newBuf[i] != ch)
+		int length = 0;
+		while (*newBuf != ch && *newBuf != '\0')
 		{
-			str[numRes] = new char[end - begin + 2];
+			newBuf++;
+			length++;
 		}
-		for (int k = 0; k < (end - begin + 1); k++)
-		{
-			str[numRes][k] = newBuf[i];
-			i++;
-		}
-		str[numRes][end - begin + 1] = '\0';
-		i++;
+		str[numRes] = new char[length + 1];
+		memcpy(str[numRes], newBuf - length, length);
+		str[numRes][length] = '\0';
 		numRes++;
 	}
 	*result = str;
