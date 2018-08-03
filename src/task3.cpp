8ae2cd3096ee8021809d309ec9797117e7c31aaa
Anton Sorokin <Tofer159@gmail.com>
2018-08-03 19:15:34 +0300

    v0.991

--- src/task3.cpp
+++ src/task3.cpp
@@ -3,8 +3,18 @@
 unsigned long long sumPrime(unsigned int hbound)
 {
 	unsigned long long sum{0};
-	for (unsigned int i = 2; i < hbound; i++)
+	int i{2};
+	while (i<hbound)
+	{
+		if (checkPrime(i) == true) sum += i;
+		i++;
+	}	
+	
+	
+/*	for (unsigned int i = 2; i < hbound; i++)
 		if (checkPrime(i) == true)
-			sum += i;
+			sum += i;*/
 	return sum;
 }
+
+
