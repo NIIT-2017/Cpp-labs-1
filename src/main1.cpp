3ec95a6cf7887ec86ab6ea9eef20cb8061c71c60
Murat9202 <40727872+Murat9202@users.noreply.github.com>
2018-08-11 00:16:33 +0400

    Lab1

---
+++ src/main1.cpp
@@ -0,0 +1,22 @@
+
+
+#include "task1.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int min = 0, max = 0;
+	while (max <= min || max > 20 || min < 1)
+	{
+		cout << "enter min" << endl;
+		cin >> min;
+		cout << "enter max" << endl;
+		cin >> max;
+	}
+	cout << "result =" << findValue(min, max) << endl;
+	return 0;
+}
+
+
