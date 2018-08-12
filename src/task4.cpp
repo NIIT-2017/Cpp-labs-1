84d1242f181b7636bd0490cf585765f04f02cba6
Murat9202 <40727872+Murat9202@users.noreply.github.com>
2018-08-13 02:55:15 +0400

    v1.2

--- src/task4.cpp
+++ src/task4.cpp
@@ -7,19 +7,58 @@ using namespace std;
 
 char *sum(char *x, char *y)
 {
-	type a, b, c;
+	string a = string(x), b = string(y), sum = "", sum2 = "";
+	int c = 0, d = 0, t = 0, n = 0;
 
-	a = (type)atoi(x);
-	b = (type)atoi(y);
+	if (a.size()>b.size())
+	{
+		t = a.size() - b.size();
+		for (n = 0; n<t; n++)
+			sum += '0';
+		sum += b;
+		b = sum;
+	}
+	if (a.size()<b.size())
+	{
+		t = b.size() - a.size();
+		for (n = 0; n<t; n++)
+			sum += '0';
+		sum += a;
+		a = sum;
+	}
+	if (a.size() == b.size())
+		sum = a;
 
-	c = a + b;
+	for (n = a.size() - 1; n >= 0; n--)
+	{
+		t = d + (b[n] - '0') + (a[n] - '0');
+		c = t % 10;
+		d = t / 10;
+		sum[n] = c + '0';
 
-	string smb = to_string(c);
+		if (n == 0 && d == 1)
+		{
+			sum2 = sum + '0';
+			for (int j = sum.size(); j >= 0; j--)
+			{
+				if (j == 0)
+				{
+					sum2[j] = 1 + '0';
+					break;
+				}
+				sum2[j] = sum[j - 1];
+			}
+			sum = sum2;
+		}
+	}
 
-	char *result = new char;
-	char *out = (char*)smb.c_str();
+	int size = sum.size();
 
-	strcpy(result, out);
+	char *result = new char[size + 1];
+	char *out = (char*)sum.c_str();
+
+	strncpy(result, out, size);
+	result[size] = '\0';
 
 	return result;
 }
