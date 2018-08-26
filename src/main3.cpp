4dee8abbfe10dc2c7e1eab548f13ca3ba29e0e2a
MashaSS <33683640+MashaSS@users.noreply.github.com>
2018-08-26 23:03:19 +0300

    update

--- src/main3.cpp
+++ src/main3.cpp
@@ -9,6 +9,5 @@ int main()
 	cout << "bound is 10: " << sumPrime(10) << endl;
 	cout << "bound is 17: " << sumPrime(500) << endl;
 	cout << "bound is 42: " << sumPrime(42) << endl;
-	system("pause");
 	return 0;
 }
