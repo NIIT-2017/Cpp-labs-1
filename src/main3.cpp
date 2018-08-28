9fe3c7a142334d13c35c6ef06de3b5553a767d47
MashaSS <33683640+MashaSS@users.noreply.github.com>
2018-08-28 16:32:48 +0300

    update

---
+++ src/main3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "task3.h"
+#include "task2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "sum 6 =" << sumPrime(6) << endl;
+	cout << "sum 8 =" << sumPrime(8) << endl;
+	cout << "sum 13 ="<<sumPrime(13) << endl;
+	cout <<"sum 2000000 ="<< sumPrime(2000000) << endl;
+	return 0;
+}
